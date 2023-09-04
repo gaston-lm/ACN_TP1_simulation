b6394663ad094273e94f2a85b7ec721edd381858
gaston-lm <gastonlm11lm@gmail.com>
2023-09-03 22:58:06 -0300

    simulacion sin aleatoriedad

--- agent.py
+++ agent.py
@@ -1,3 +1,6 @@
+import numpy as np
+import copy
+
 class Agent:
     def __init__(self, id, position, speed, acceleration):
         self.pos = position
@@ -5,33 +8,66 @@ class Agent:
         self.acc = acceleration
         self.id = id
 
-    def update_acc(self, neighbour, max_spd):
-        dist = neighbour.pos - self.pos
-        relation_time_dist = dist / self.spd
-        
-        new_acc = self.acc
+    def update_first(self):
+        # agregar condicion si le importa la max_speed
+        prev_spd = copy.deepcopy(self.spd)
+        prev_pos = copy.deepcopy(self.pos)
+        prev_acc = copy.deepcopy(self.acc)
 
-        if relation_time_dist > 2:
-            new_acc += 0 # ver magnitud
-        elif relation_time_dist < 2:
-            new_acc -= (self.acc + 0) # ver magnitud --> cierta f(max_speed, neighbour.acc)
+        # De Liniers a Acceso Norte es 80km/h
+        max_spd:float = 22.22 # 80 km/h en m/s
+        if prev_pos > 10000:
+            max_spd = 27.77 # 100 km/h
 
-        # si veo las luces rojas del adelante está frenando, lo podríamos ver con su acc negativa?        
+        self.pos = prev_pos + prev_spd * 1 + 0.5 * prev_acc * 1**2 # 1s frac de tiempo
+
+        self.spd = prev_spd + prev_acc * 1 # 1s frac de tiempo
+
+        new_acc = 3.5 # acelero según aceleración según autos promedio
+
+        if self.spd + new_acc < max_spd:
+            self.acc = new_acc # + random error * Indicadora (se distrajo o no) --> poisson?
+        else:
+            self.acc = max_spd - self.spd
+
+    def update(self, self_prev_pos, self_prev_spd, self_prev_acc, neigbour_prev_pos, neigbour_prev_spd, alpha, l, m):
+        prev_spd = self_prev_spd
+        prev_pos = self_prev_pos
+        prev_acc = self_prev_acc
+
+        sensitivity_coeff = (neigbour_prev_pos - prev_pos) / prev_spd
+        t = np.random.exponential(scale=100, size=1).item()
+
+        # De Liniers a Acceso Norte es 80km/h
+        max_spd:float = 22.22 # 80 km/h en m/s
+        if prev_pos > 10000:
+            max_spd = 27.77 # 100 km/h
+
+        # update pos
+        self.pos = prev_pos + prev_spd * 1 + 0.5 * prev_acc * 1**2 # 1s frac de tiempo
+
+        # update spd
+        self.spd = prev_spd + prev_acc * 1 # 1s frac de tiempo
+
+        print(neigbour_prev_pos, prev_pos)
+        # if abs(neigbour_prev_pos - prev_pos) < 0.01:
+            
+
+        # update acc
+        # new_acc = 0
+        # if sensitivity_coeff > t:
+        #     new_acc = 3.5 # acelero según aceleración según autos promedio
+        # elif sensitivity_coeff < t:
+        #     # formula "segura"
+        new_acc = ((alpha * prev_spd**m) / (neigbour_prev_pos - prev_pos)**l) * (neigbour_prev_spd - prev_spd)
+
+        # no es real que pueda acelerar en 1s en más de 3.5 m/s
+        if new_acc > 3.5:
+            new_acc = 3.5
 
         if self.spd + new_acc <= max_spd:
             self.acc = new_acc # + random error * Indicadora (se distrajo o no) --> poisson?
+        else:
+            self.acc = max_spd - self.spd
 
-    def update_pos(self):
-        self.pos = self.pos + self.spd # * time 
-
-    def update_spd(self):
-        self.spd = self.spd + self.acc # * time 
-
-    def update(self, neighbour):
-        self.update_spd()
-        self.update_pos()
-        # De Liniers a Acceso Norte es 80km/h
-        max_spd:float = 22.22 # 80 km/h en m/s
-        if self.pos > 10000:
-            max_spd = 27.77 # 100 km/h
-        self.update_acc(neighbour, max_spd)
+        # si veo las luces rojas del adelante está frenando, lo podríamos ver con su acc negativa?
--- lane.py
+++ lane.py
@@ -3,24 +3,61 @@ from agent import *
 from typing import List
 
 class Lane_simulation:
-    def __init__(self, time_limit, agents):
+    def __init__(self, time_limit, agents, alpha, l, m):
         # Pre: agents <= time 
         self.time:int = 0
         self.time_limit:int = time_limit
         self.agents:int = agents
 
-        self.pos:List[List[float]] = np.array(([[-1]*agents])*time_limit)
-        self.acc:List[List[float]] = np.array(([[-1]*agents])*time_limit)
-        self.spd:List[List[float]] = np.array(([[-1]*agents])*time_limit)
+        self.pos:List[List[float]] = np.array(([[0.0]*agents])*time_limit)
+        self.acc:List[List[float]] = np.array(([[0.0]*agents])*time_limit)
+        self.spd:List[List[float]] = np.array(([[0.0]*agents])*time_limit)
 
-        self.lane:List[Agent] = []
+        # self.lane:List[Agent] = []
+
+        self.alpha = alpha
+        self.l = l
+        self.m = m
 
     def enter(self, a:int, t:int):
-        spd:float = np.random.uniform(low=8.33, high=16.66, size=1).item() # Entre 30 y 60 km/h
-        self.lane.append(Agent(id=a, position=0, speed=spd, acceleration=0))
-        self.pos[a,t] = 0
+        spd:float = np.random.uniform(low=8.33, high=16, size=1).item() # Entre 30 y 60 km/h
+        # self.lane.append(Agent(id=a, position=0, speed=spd, acceleration=0))
+        self.pos[a,t] = 1
         self.spd[a,t] = spd
         self.acc[a,t] = 0
+
+    def update(self, t):
+        i = 0
+        while i < len(self.acc[:t]):
+            # update pos
+            self.pos[i,t] = self.pos[i,t-1] + self.spd[i,t-1] * 1
+
+            # update spd
+            self.spd[i,t] = self.spd[i,t-1] + self.acc[i,t-1] * 1
+
+            # update acc
+
+            # De Liniers a Acceso Norte es 80km/h
+            max_spd:float = 22.22 # 80 km/h en m/s
+            if self.pos[i,t-1] > 10000:
+                max_spd = 27.77 # 100 km/h
+
+            if i == 0:
+                new_acc = 3.5
+            else:
+                new_acc = ((self.alpha * self.spd[i,t-1]**self.m) / (self.pos[i-1,t-1] - self.pos[i,t-1])**self.l) * (self.spd[i-1,t-1] - self.spd[i,t-1])
+            
+            if new_acc > 3.5:
+                new_acc = 3.5
+            elif new_acc < -3.5:
+                new_acc = -3.5
+
+            if self.spd[i,t] + new_acc <= max_spd:
+                self.acc[i,t] = new_acc # + random error * Indicadora (se distrajo o no) --> poisson?
+            else:
+                self.acc[i,t] = max_spd - self.spd[i,t]
+            
+            i += 1
     
     def simulation(self):
         agents_enter = 0
@@ -29,14 +66,27 @@ class Lane_simulation:
                 # Con alguna proba:
                 self.enter(agents_enter, t)
                 agents_enter += 1
+            
+            if t == 0:
+                continue
+            else:
+                self.update(t)
 
-            for agent in self.lane:
-                self.pos[agent.id,t] = agent.pos
-                self.spd[agent.id,t] = agent.spd
-                self.acc[agent.id,t] = agent.acc
+            # for agent in self.lane:
+            #     self.pos[agent.id,t] = agent.pos
+            #     self.spd[agent.id,t] = agent.spd
+            #     self.acc[agent.id,t] = agent.acc
 
-                # Si ya termino el viaje, borramos al Agent.
-                if agent.pos > 12700:
-                    self.lane.remove[0] # Eliminamos el primero (el que iba adelante y llegó).
+            #     # Si ya termino el viaje, borramos al Agent.
+            #     if agent.pos > 12700:
+            #         self.lane.pop(0) # Eliminamos el primero (el que iba adelante y llegó).
 
-                agent.update(self.lane[agent.id - 1]) # Otra función para el primero? Es el único que no necesita al vecino. Otra opción sería que cada agent tenga almacenado a su vecino.
+            #     neigbour_prev_pos = self.pos[agent.id - 1, t - 1]
+            #     neigbour_prev_spd = self.spd[agent.id - 1, t - 1]
+            #     self_prev_pos = self.pos[agent.id, t-1]
+            #     self_prev_spd = self.spd[agent.id, t-1]
+            #     self_prev_acc = self.acc[agent.id, t-1]
+            #     if agent.id == 0:
+            #         agent.update_first()
+            #     else:
+            #         agent.update(self_prev_pos, self_prev_spd, self_prev_acc, neigbour_prev_pos, neigbour_prev_spd, self.alpha, self.l, self.m) # Otra función para el primero? Es el único que no necesita al vecino. Otra opción sería que cada agent tenga almacenado a su vecino.
