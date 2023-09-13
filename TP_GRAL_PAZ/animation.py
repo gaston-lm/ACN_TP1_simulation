7066ef746d62ad097e21fe9ffc5b321ad83050a4
FedeeGiorgi <fgiorgi@mail.utdt.edu>
2023-09-12 23:51:47 -0300

    ENTREGA

---
+++ TP_GRAL_PAZ/animation.py
@@ -0,0 +1,124 @@
+import pygame
+import numpy as np
+import os
+import sys
+
+class Video:
+
+    def __init__(self,size):
+        self.path = "D:\\Facultad\\animation"
+        self.name = "capture"
+        self.cnt = 0
+
+        try:
+            os.makedirs(self.path)
+        except OSError:
+            pass
+    
+    def make_png(self,screen):
+        self.cnt+=1
+        fullpath = self.path + "\\"+self.name + "%08d.png"%self.cnt
+        pygame.image.save(screen,fullpath)
+
+    #https://stackoverflow.com/questions/44947505/how-to-make-a-movie-out-of-images-in-python
+    #https://stackoverflow.com/questions/3561715/using-ffmpeg-to-encode-a-high-quality-video
+    def make_mp4(self):
+        os.system("ffmpeg -r 60 -i D:\Facultad\\animation\\capture%08d.png -vcodec mpeg4 -q:v 0 -y movie.mp4")
+
+
+# Initialize Pygame
+pygame.init()
+
+# Define constants for the screen dimensions and colors
+WIDTH, HEIGHT = 1600, 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Define the road length in meters
+ROAD_LENGTH_METERS = 15500
+
+# Create a Pygame window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Car Animation")
+
+# Function to draw the road and position labels
+def draw_road():
+    # Draw the road as two horizontal lines
+    # pygame.draw.line(screen, BLACK, (0, HEIGHT // 2), (WIDTH, HEIGHT // 2), 5)
+
+    # Draw position labels above the road (rounded to kilometers)
+    label_interval = ROAD_LENGTH_METERS / 10
+    for i in range(11):
+        position_km = int(i * label_interval / 1000)  # Convert to rounded kilometers
+        label = f"{position_km} km"
+        font = pygame.font.Font(None, 24)  # Smaller font size
+        text = font.render(label, True, BLACK)
+        text_rect = text.get_rect()
+        text_rect.center = (i * WIDTH // 10, HEIGHT // 4 - 20)  # Adjusted position
+        if i == 0:
+            text_rect.left = 10  # Adjust for 0 km label
+        elif i == 10:
+            text_rect.right = WIDTH - 10  # Adjust for 15 km label
+        screen.blit(text, text_rect)
+
+# Function to draw cars at their respective positions
+def draw_cars(positions, time_step):
+    # Clear the screen
+    screen.fill(WHITE)
+
+    # Draw the road and position labels
+    draw_road()
+
+    # Draw cars as rectangles
+    num_cars = positions.shape[0]
+    car_width = 3
+    car_height = 3
+    for i in range(num_cars):
+        car_x_percent = positions[i, time_step] / ROAD_LENGTH_METERS
+        car_x = car_x_percent * WIDTH
+        car_y = HEIGHT // 2 - car_height // 2
+        pygame.draw.rect(screen, BLACK, (car_x, car_y, car_width, car_height))
+
+    # Display the current time
+    font = pygame.font.Font(None, 36)
+    time_label = f"Time: {time_step}"
+    text = font.render(time_label, True, BLACK)
+    screen.blit(text, (10, 10))
+
+    # Update the display
+    pygame.display.flip()
+
+def main(positions, fps):
+    num_time_steps = positions.shape[1]
+    running = True
+    time_step = 0
+
+    # Create a Pygame clock to control the frame rate
+    clock = pygame.time.Clock()
+    video = Video((1280,720))
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        video.make_png(screen)
+
+        draw_cars(positions, time_step)
+        time_step = (time_step + 1) % num_time_steps
+
+        # Control the frame rate
+        clock.tick(fps)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    positions = np.load("npys/results_noche_pasada/pos0.npy")
+
+    # Set the desired FPS for animation
+    animation_fps = 60
+
+    main(positions, animation_fps)
+
+    video = Video((1280,720))
+    video.make_mp4()
---
+++ TP_GRAL_PAZ/road.py
@@ -0,0 +1,365 @@
+import numpy as np
+
+class RoadSimulation:
+    def __init__(self, time_limit, a_max, b, delta, s_0, car_length, alert_app_prop):
+        # Public
+        self.time_limit = time_limit
+
+        self.pos = np.zeros((1,time_limit))
+        self.acc = np.zeros((1,time_limit))
+        self.spd = np.zeros((1,time_limit))
+        self.a_max = a_max
+        self.delta = delta
+        self.b = b
+        self.s_0 = s_0
+        self.car_length = car_length
+
+        # Private
+        # Para cada agente
+        self.dsr_spd = []
+        self.headway_mean = []
+        self.headway = []
+
+        # Para el análisis de resultados
+        self.time_out = []
+        self.time_in = []
+        self.arrived = set()
+
+        # Para manejar las colisiones
+        self.collisioned = []
+        self.collisioned_agents = set()
+        self.collisions = dict()
+        self.collisions_in_radar = dict()
+        self.actual_collisions = dict()
+
+        # Results
+        self.results = np.empty((0,0))
+        self.avg_travel_time = 0
+        self.avg_travel_speed = 0
+        self.avg_travel_acc = 0
+
+        # Para calcular stats después que llegó el primer auto
+        # Flag de primer auto llegó
+        self.flag_first_arrived = False
+        # Id del primer auto 
+        self.id_first_agent_to_consider = -1
+
+        # For cool finding / hipótesis
+        self.alert_app_prop = alert_app_prop
+        self.uses_alert_app = []
+
+    def agent_attr(self):
+        # Headway
+        hdw = np.random.lognormal(mean=0.5, sigma=0.21)
+        # Media de headway para este agente
+        self.headway_mean.append(hdw)
+        # Donde se registra el headway con cierta variabilidad cada 30 segundos
+        self.headway.append(hdw)
+
+        # Velocidad deseada
+        desired_speed_for_agent = 0
+        if hdw < 1.2:
+            # Son loquitos
+            desired_speed_for_agent = 22.22 + np.random.lognormal(mean=1.5, sigma=0.25)
+        elif 1.2 < hdw < 1.5:
+            # Van más rápido
+            desired_speed_for_agent = 22.22 + np.random.uniform(low=0, high=4)
+        elif 1.5 < hdw < 1.8:
+            # Van normal
+            desired_speed_for_agent = 21.80 + np.random.normal(loc=0, scale=0.4)
+        else:
+            # Vam más lento
+            desired_speed_for_agent = 19.44 + np.random.normal(loc=0, scale=0.8)
+       
+        self.dsr_spd.append(desired_speed_for_agent)
+
+        # Usa o no app de alerta de radares
+        p = np.random.uniform(low=0, high=1)
+        if p > self.alert_app_prop:
+            self.uses_alert_app.append(True)
+        else:
+            self.uses_alert_app.append(False)
+
+    def enter(self, a, t):
+        if t != 0:
+            new_agent = np.zeros((1,self.time_limit))
+            self.pos = np.vstack((self.pos, new_agent))
+            self.acc = np.vstack((self.acc, new_agent))
+            self.spd = np.vstack((self.spd, new_agent))
+
+        # Definimos atributos del agente entrando
+        self.agent_attr()
+
+        # Vector para registrar tiempo a esperar si choco
+        self.collisioned.append(-1)
+
+        self.pos[a,t] = 0.5
+
+        # Defino acc segun quien veo adelante.
+        if t == 0:
+            spd = np.random.uniform(low=8.33, high=9.72, size=1) # Entre 30 y 45 km/h
+            self.spd[a,t] = spd
+            self.acc[a,t] = self.a_max * (1 - (spd / 22.22) ** self.delta)
+
+        else:
+            spd_prev = np.random.uniform(low=8.33, high=9.72, size=1)
+            acc_prev = min(2, max(-4 , self.a_max * (1 - (spd_prev / 22.22) ** self.delta - ((self.s_0 + spd_prev * self.headway[a] + (spd_prev * (spd_prev - self.spd[a-1, t])) / (2 * np.sqrt(self.a_max * self.b))) / (self.pos[a-1, t] - 1 - self.car_length)) ** 2)))
+        
+            spd = spd_prev + acc_prev
+                
+            self.spd[a,t] = spd
+            self.acc[a,t] = min(2, max(-4, self.a_max * (1 - (spd / 22.22) ** self.delta - ((self.s_0 + spd * self.headway[a] + (spd * (spd - self.spd[a-1, t])) / (2 * np.sqrt(self.a_max * self.b))) / (self.pos[a-1, t] - 1 - self.car_length)) ** 2)))
+
+        # Registro entrada de autos a partir de llegado el primero
+        if self.flag_first_arrived:
+            self.time_in.append(t)
+            # Tomo agentes para salida a partir de este id (es el primero que se empieza a tener en cuenta)
+            if self.id_first_agent_to_consider == -1:
+                self.id_first_agent_to_consider = a
+    
+    def update_pos(self, i, t):
+        pos_noise = 0
+        # Si este auto choco, no le puedo sumar ruido, debe bajar o mantenerse en 0
+        if self.collisioned[i] != -1 and self.spd[i,t] > 0: 
+            pos_noise = np.random.normal(loc=0,scale=0.6)
+
+        self.pos[i, t] = pos_noise + self.pos[i, t-1] + self.spd[i, t-1] * 1 # unidad de tiempo (1s)
+
+    def update_spd(self, i, t):
+        spd_noise = 0
+        # Si este auto choco, no le puedo sumar ruido, debe bajar o mantenerse en 0
+        if self.collisioned[i] != -1 and self.spd[i,t] > 0: 
+            spd_noise = np.random.normal(loc=0,scale=0.3)
+
+        self.spd[i, t] = max(0.0, spd_noise + self.spd[i, t-1] + self.acc[i, t-1] * 1) # unidad de tiempo (1s) 
+
+    def update_acc(self, i, t, v_0):
+        v = self.spd[i, t]
+        # Si soy el lider, acelero sin tener en cuenta al auto de adelante (no hay auto de adelante)
+        if i == 0:
+            acc = self.a_max * (1 - (v / v_0) ** self.delta)
+        # Si no, acelero teniendo en cuenta mi distancia con el conductor de adelante y a cuantos segundos quiero estar del mismo, la velocidad deseada, y mas parámetros del modelo.
+        else:
+            s = self.pos[i-1, t] - self.pos[i, t] - self.car_length
+            s_star = self.s_0 + v * self.headway[i] + (v * (v - self.spd[i-1, t])) / (2 * np.sqrt(self.a_max * self.b))
+            acc = self.a_max * (1 - (v / v_0) ** self.delta - (s_star / s) ** 2)
+        
+        # Ruido acc
+        acc_noise = np.random.normal(loc=0,scale=0.15)
+        acc = acc + acc_noise
+
+        # Límites físicos de la aceleración:
+        if acc < -self.b:
+            acc = -self.b
+        elif acc > self.a_max:
+            acc = self.a_max
+
+        # Distracciones y actualización de la matriz.
+        indicadora = np.random.uniform(low=0, high=1)
+        lm = 0.02
+
+        if indicadora < 0.0005:
+            # Distracción "fuerte"
+            self.acc[i,t] = acc - np.random.lognormal(mean=1.1, sigma=0.1)
+        elif indicadora < lm:
+            # Distación "leve"
+            # Se distrajo y no modifica la aceleración anterior.
+            self.acc[i,t] = self.acc[i, t-1]
+        else:
+            # Modifico la aceleracion acorde al modelo.
+            self.acc[i,t] = acc
+
+    def is_in_radar_window(self, i, t):
+        vr = False
+
+        # Hay radares en: 2.4k, 6.5k, 12.2k
+        radar_windows = [(2000, 2550),(6000, 6650),(11700, 12350)]
+        
+        if any(start < self.pos[i, t] < end for start, end in radar_windows):
+            vr = True
+
+        return vr
+
+    def dsr_spd_based_on_pos(self, i, t, v_0):
+        new_v_0 = v_0
+        # Si pasó Acceso Norte, dónde la velocidad máxima pasa 100km/h
+        if self.pos[i, t-1] > 13000:
+            diferencia = 22.22 - v_0 # Si v_0 > 22.22 le gusta ir más rápido, su nueva v_0 también es mayor que 27.77.
+            new_v_0 = 27.77 - diferencia 
+
+        if self.is_in_radar_window(i, t-1):
+            if self.uses_alert_app[i] and v_0 > 21.5:
+                new_v_0 = 21 + np.random.normal(loc=0 , scale=0.06)
+
+        return new_v_0
+    
+    def dsr_spd_change(self, i, t):
+        # Calculamos su desired speed basado en sus caracteristicas de conductor + pos
+        v_0 = self.dsr_spd_based_on_pos(i, t, self.dsr_spd[i])
+
+        # Con cierta proba le sumamos ruido
+        p = np.random.uniform(low=0, high=1)
+        v_0_noise = 0
+        
+        if p > 0.5:
+            v_0_noise = np.random.normal(loc=0, scale=1.5)
+
+        return v_0 + v_0_noise
+    
+    def update_headway(self, i, t):
+        if any((0 <= pos_collision - self.pos[i,t]  <= 500 and time + 3 == t) for time, pos_collision in self.actual_collisions.values()):
+            self.headway[i] = self.headway_mean[i] + 1.0
+
+        elif any((0 <= pos_collision - self.pos[i,t]  <= 500) for _, pos_collision in self.actual_collisions.values()):
+            pass
+
+        else:
+            if t % 30 == 0:
+                self.headway[i] = self.headway_mean[i]
+                p = np.random.uniform(low=0, high=1)
+                if p > 0.60:
+                    self.headway[i] = self.headway_mean[i] + np.random.normal(loc=0, scale=0.3)
+    
+    def verify_colision(self, i, t):
+        if self.collisioned[i] != -1 and t < self.collisioned[i]: # Si este auto choco, verifico que el tiempo para frenar siga vigente para desacelerar manera realista
+            if self.spd[i,t] > 0.5: # Si aun me queda por frenar
+                self.acc[i,t] = -self.b / 2 # no freno tan brusco
+            else:
+                self.acc[i,t] = 0
+                self.spd[i,t] = 0
+                self.pos[i,t] = self.pos[i, t-1]
+            
+        elif self.collisioned[i] == 0:
+            # Ya paso el tiempo, vuelve a arrancar
+            self.acc[i,t] = 1.67 # Ligeramente menor a acc max para que luego de un choque no arranquen lo más rápido
+            self.collisioned[i] = -1
+            self.collisioned_agents.remove(i)
+            self.actual_collisions.pop(i)
+            
+    def identify_colision(self, i, t):
+        if (i != 0) and (i not in self.collisioned_agents) and (self.pos[i,t] < 15500):
+            if self.pos[i-1,t] - self.car_length <= self.pos[i,t]: 
+                if self.pos[i,t-4] < 2:
+                    print("Recien había entrado y ya choque :(")
+                
+                print("Choque de " + str(i) + " con " + str(i-1) + " en t = " + str(t) + " en " + str(self.pos[i,t]))  
+
+                # Registrar choque y agentes involucrados
+                self.collisioned_agents.add(i)
+                self.collisioned_agents.add(i-1)
+                self.collisioned[i] = t + (self.spd[i,t] // self.b) + 120 # Sumamos tiempo que requerira frenarse por completo + tiempo para pasarse datos del seguro
+                self.collisioned[i-1] = t + (self.spd[i-1,t] // self.b) + 120 
+
+                if self.is_in_radar_window(i, t):
+                    self.collisions_in_radar[i] = (t, self.pos[i,t])
+                    self.collisions_in_radar[i-1] = (t, self.pos[i-1,t])
+                else:
+                    self.collisions[i] = (t, self.pos[i,t])
+                    self.collisions[i-1] = (t, self.pos[i-1,t])
+                
+                self.actual_collisions[i] = (t, self.pos[i,t])
+
+    def update(self, t):
+        i = 0
+        while i < len(self.pos[:,t]):
+            # Velocidad deseada del conductor dado por sus caracteristicas + max_spd en ese lugar + ruido
+            v_0 = self.dsr_spd_change(i, t)
+
+            # Con cierta proba y frecuenta modifica su headway
+            self.update_headway(i, t)
+            
+            # Actualización de la posición y velocidad en el segundo t. Física, no es una decisión del agente.
+            self.update_pos(i, t)
+            self.update_spd(i, t)
+            
+            # Actualización de la aceleración
+            self.update_acc(i, t, v_0)
+
+            # Manejo las colisiones
+            self.verify_colision(i, t)
+            self.identify_colision(i, t)
+            
+            # Registro de llegada de agentes
+            if self.pos[i, t] >= 15500 and i not in self.arrived:
+                self.arrived.add(i)
+                if i == 0:
+                    self.flag_first_arrived = True
+                    print("El primero llego en t="+str(t))
+                    print("Cantidad de autos ahi: "+str(len(self.pos[:,t])))
+                elif self.id_first_agent_to_consider != -1 and self.id_first_agent_to_consider <= i:
+                    print(i,t)
+                    self.time_out.append(t)
+
+            i += 1
+
+    def simulate(self):
+        agents_enter = 0
+        self.enter(agents_enter,0)
+        t = 1
+        agents_enter = 1
+
+        while t < self.time_limit:
+            self.update(t)
+            p = np.random.uniform(low=0, high=1, size=1).item()
+            # Definición de umbral de entrada en base a distintos horarios a simular
+            umbral = 0 
+            if 0 < t < 4400: # de 5 a 6
+                umbral = 0.9
+            elif 4400 < t < 8000: # de 6 a 7
+                umbral = 0.5
+            elif 8000 < t < 11600: # de 7 a 8
+                umbral = 0
+            elif 11600 < t < 15200: # de 8 a 9
+                umbral = 0.2
+            elif 15200 < t < 18800: # de 9 a 10
+                umbral = 0.4
+            else:
+                umbral = 0.5
+
+            if self.pos[agents_enter-1,t] > 10 and p > umbral:
+                self.enter(agents_enter, t)
+                agents_enter += 1
+            
+            t += 1
+    
+    def generate_results(self):
+        self.time_out = np.array(self.time_out)
+        self.time_in = np.array(self.time_in[:self.time_out.shape[0]])
+        
+        self.results = np.vstack((self.time_in, self.time_out))
+
+        # Calcular tiempo promedio de viaje
+        travel_time = self.time_out - self.time_in
+
+        self.results = np.vstack((self.results, travel_time))
+        self.avg_travel_time = np.mean(travel_time)
+
+        # Calcular velocidad promedio
+        spds = []
+        for i in range(len(self.time_out)):
+            spd_agent = self.spd[self.id_first_agent_to_consider + i][self.time_in[i]:self.time_out[i]]
+            spds.append(np.mean(spd_agent))
+
+        self.results = np.vstack((self.results, np.array(spds)))
+        self.avg_travel_speed = np.sum(spds) / len(self.time_out)
+
+        # Calcular aceleración promedio
+        accs = []
+        for i in range(len(self.time_out)):
+            acc_agent = self.acc[self.id_first_agent_to_consider + i][self.time_in[i]:self.time_out[i]]
+            accs.append(np.mean(acc_agent))
+
+        self.results = np.vstack((self.results, np.array(accs)))
+        self.avg_travel_acc = np.sum(accs) / len(self.time_out)
+        
+    def get_avg_travel_time(self):
+        return self.avg_travel_time
+
+    def get_avg_travel_speed(self):
+        return self.avg_travel_speed
+
+    def get_avg_travel_acce(self):
+        return self.avg_travel_acc
+
+    def get_collisions(self):
+        return len(self.collisions) + len(self.collisions_in_radar)
